3aed84493a05b8e2f25c4a66dc55c68297496724
leogamer0201 <42124209+leogamer0201@users.noreply.github.com>
2018-09-01 18:55:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -187,8 +187,7 @@ if (command == `${prefix}anunciar`) {
         }
 	    
   if(command === "ping") {
-       const m = await message.channel.send("Ping?");
-       m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+       const m = await message.channel.send("Ping?"); m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
        }
 	    
     });
