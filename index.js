cb9e3c2f2d2af696fe04025846694dff4aeef7e1
leogamer0201 <42124209+leogamer0201@users.noreply.github.com>
2018-09-01 19:08:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -186,9 +186,11 @@ if (command == `${prefix}anunciar`) {
           kickchannel.send(kickEmbed);
         }
 	    
-  if(command === "${Math.round(client.ping)}ms") {
-m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-       }
+  if(command === "ping") {
+    const m = await message.channel.send("${Math.round(client.ping)}ms");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+
 	    
     });
 bot.login(TOKEN);
