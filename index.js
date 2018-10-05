932c5e41c69eeb2bb77a09844ddd6c3b33d3e6b4
leogamer0201 <42124209+leogamer0201@users.noreply.github.com>
2018-10-04 23:12:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -142,22 +142,5 @@ if (command == `${prefix}anunciar`) {
           reportschannel.send(reportEmbed);
         } 
 
-  if (command == `${prefix}botinfo`) {
-module.exports.run = async (bot, message, args) => {
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt);
-
-    message.channel.send(botembed);
-}
-
-module.exports.help = {
-  name:"botinfo"
-}
-
     });
 bot.login(TOKEN);
