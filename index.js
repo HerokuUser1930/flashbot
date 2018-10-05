3edefeb48dba367fb82f612612988a9f9b71631e
leogamer0201 <42124209+leogamer0201@users.noreply.github.com>
2018-10-04 23:45:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -148,19 +148,18 @@ else if (message.content === `${prefix}server`) {
 
 client.on("guildMemberAdd", (member) => {
   const guild = member.guild;
-  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+ else if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
   newUsers[guild.id].set(member.id, member.user);
 
-  if (newUsers[guild.id].size > 10) {
+ else if (newUsers[guild.id].size > 10) {
     const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
     guild.channels.find("name", "general").send("Bem-vindo nossos novos usuários!\n" + userlist);
     newUsers[guild.id].clear();
   }
-});
  
 client.on("guildMemberRemove", (member) => {
   const guild = member.guild;
-  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+ else if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
 
     });
 bot.login(TOKEN);
