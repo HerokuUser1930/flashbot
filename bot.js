358116015cc3dee8b074a225462e433dc3f9c99f
HerokuUser1930 <42124061+HerokuUser1930@users.noreply.github.com>
2018-11-03 17:56:16 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,21 @@ var bot = new Discord.Client()
 
 const TOKEN = process.env.BOT_TOKEN
 
-bot.on("message", function(message) {
+bot.on('guildMemberAdd', (member) => {
+  let avatar = member.user.avatarURL
+
+  let role = member.guild.roles.find('name', ':zipper_mouth: NÃO VERIFICADO');
+
+  let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setThumbnail(avatar)
+      .addField('Bem vindo ao discord.', `Bem vindo(a) ${member} ao discord oficial do Atlantic!\n Você foi o __${member.guild.memberCount}__ player a entrar em nosso servidor\n \nPara interagir com os player vá em: #:thought_balloon:chat\nPara ver os nossos anúncios vá em: #:mega:avisos\n \nAcesse já o servidor: mc-atlantic.tk`)
+      .setFooter(`Atlantic`);
+      member.addRole(role)
+    let channel = message.guild.channels.find("name", "bemvindo")
+    channel.send(embed)
+
+}
 
 bot.on('ready', () => {
 
