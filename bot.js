a337dd63747d85c5785f74ab152739847bd09271
leogamer0201 <42124209+leogamer0201@users.noreply.github.com>
2018-11-11 04:36:34 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -247,7 +247,7 @@ if (cmd == `${prefix}new`) {
     const reason = message.content.split(" ").slice(1).join(" ");
     if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`Este servidor não tem um cargo chamado \`Support Team\` então o ticket não será aberto.\nSe você for um administrador, crie um exatamente com esse nome e dê a ele usuários que possam ver os tickets.`);
     if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`Você já tem um ticket aberto.`);
-    message.guild.createChannel(`ticket-'01', '02'`, "text").then(c => {
+    message.guild.createChannel(`ticket-`, '01', '02').then(c => {
         let role = message.guild.roles.find("name", "Support Team");
         let role2 = message.guild.roles.find("name", "@everyone");
         c.overwritePermissions(role, {
@@ -268,7 +268,6 @@ if (cmd == `${prefix}new`) {
 	.setAuthor(message.author.tag, message.author.displayAvatarURL)
         .addField(`Prezado, ${message.author.username}!`, `Por favor, tente explicar por que você abriu este ticket com o máximo de detalhes possível. Nossa ** Equipe de suporte ** estará aqui em breve para ajudar.`)
         .setAuthor(message.author.tag, message.author.displayAvatarURL)
-        message.content[prefix:]
         .addField('Ticket:', `${message}`)
         .setAuthor(message.author.tag, message.author.displayAvatarURL)
         .setTimestamp();
