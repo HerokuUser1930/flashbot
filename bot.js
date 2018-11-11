eab602a93234b92af3cd1252cd73b765ccec619f
leogamer0201 <42124209+leogamer0201@users.noreply.github.com>
2018-11-11 03:28:23 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -243,7 +243,7 @@ msg1.react('👎')
 })
   }
 
-if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+if (cmd == `${prefix}new`) {
     const reason = message.content.split(" ").slice(1).join(" ");
     if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
     if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
