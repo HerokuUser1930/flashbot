0ac8f01d9976adccbb06b48e1a96f4b221b43dfc
leogamer0201 <42124209+leogamer0201@users.noreply.github.com>
2018-11-11 05:50:55 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ if (cmd == `${prefix}ajuda`) {
 			.setAuthor(message.author.tag, message.author.displayAvatarURL)
 			.addField('Comandos para Moderação:', 'F!banir - Bane o usuário do servidor (Crie um canal chamado 🚫puniçoes🚫 Para enviar a punição para este canal)')
 			.setAuthor(message.author.tag, message.author.displayAvatarURL)
-			.addField('Outros Comandos:', 'F!anunciar - Fazer um anuncio (Crie um canal chamado 🚨avisos🚨 para enviar os avisos)\nF!botinfo Ver as informações do bot\nF!enquete Fazer uma enquete no canal `⚡enquete⚡` (Crie um canal chamado ⚡enquete⚡)\nF!new (Abrir um ticket)\nF!close (Feichar um ticket)')
+			.addField('Outros Comandos:', 'F!anunciar - Fazer um anuncio (Crie um canal chamado 🚨avisos🚨 para enviar os avisos)\nF!botinfo Ver as informações do bot\nF!enquete Fazer uma enquete no canal `⚡enquete⚡` (Crie um canal chamado ⚡enquete⚡)\nF!new (Abrir um ticket)\nF!close (Feichar um ticket)\nF!yt (Procurar algo no youtube)')
                         .addField('Diverção:', 'F!ship Shippar dois usuarios')
 			.setAuthor(message.author.tag, message.author.displayAvatarURL)
 	
